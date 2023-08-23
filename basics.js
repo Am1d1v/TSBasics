6701f01646586d72181acff08eaeb4b049b3fd0e
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-23 16:59:13 +0300

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,24 @@
+// Primitives
+// Numbers
+var x = 10;
+//let y = 123n;
+var q = NaN;
+console.log(typeof (q));
+// String
+var str1 = 'Hello';
+//let symb = Symbol('qwerty');
+// Boolean
+var w = true;
+w = false;
+console.log(w);
+// Nothing
+var h = undefined;
+var g = null;
+// Literal
+var n = 100;
+var str2 = 'something';
+console.log(typeof (n));
+console.log(typeof (str2));
+// Universal
+var ii = 1;
+ii = 'str';
---
+++ basics.ts
@@ -0,0 +1,35 @@
+
+
+// Primitives
+
+
+// Numbers
+let x = 10;
+//let y = 123n;
+let q = NaN;
+console.log(typeof(q));
+
+// String
+let str1 = 'Hello';
+//let symb = Symbol('qwerty');
+
+// Boolean
+let w = true;
+w = false;
+console.log(w);
+
+// Nothing
+let h: undefined = undefined;
+let g: null = null;
+
+// Literal
+const n = 100;
+const str2 = 'something';
+console.log(typeof(n));
+console.log(typeof(str2));
+
+// Universal
+let ii: any = 1;
+ii = 'str';
+
+
