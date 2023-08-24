bee98c5f776ea4427e15b7e0da3b64fde51fe9a3
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-25 00:06:42 +0300

    Add files via upload

--- advanced.js
+++ advanced.js
@@ -1,6 +1,1 @@
-var user1 = {
-    email: 'a@a.com',
-    login: 'a',
-    password: '123'
-};
-//const person1: Employee = {};
+// Interfaces
--- advanced.ts
+++ advanced.ts
@@ -1,4 +1,7 @@
 
+// Interfaces
+
+/*
 interface IUser {
     readonly email: string;
     readonly login: string;
@@ -27,7 +30,15 @@ interface Employee extends IUser, IPerson {
     contractStart: Date,
 }
 //const person1: Employee = {};
+*/
 
+// Aliases
 
+type Union1 = 'a' | 'b' | 'c';
+type Union2 = 'a' | 'b' |'q' | 'w' | 'e';
+type Union3 = Union1 | Union2;
 
+type Union4 = 'r' | 't';
+type Union5 = 'r' | 'y';
+type Union6 = Union4 & Union5;
 
